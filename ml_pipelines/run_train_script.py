de8da02992adb2938e282ffd7260972d67534673
eholgersson <emanuel.holgersson@gmail.com>
2022-06-01 07:37:08 +0200

    use aml compute func added to run train script

--- ml_pipelines/run_train_script.py
+++ ml_pipelines/run_train_script.py
@@ -1,6 +1,8 @@
 from azureml.core import Environment, Workspace, Experiment
 from ml_pipelines.utils import EnvironmentVariables, get_environment
 from azureml.core import ScriptRunConfig
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
 
 ws = Workspace.from_config()
 env_vars = EnvironmentVariables()
@@ -17,10 +19,30 @@ except Exception as e:
     )
     environment.register(ws)
 
-environment.python.user_managed_dependencies = True
+# Uncomment when running locally
+#environment.python.user_managed_dependencies = True
+
+cpu_cluster_name = "my-cool-cluster"
+try:
+    cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(
+        vm_size="Standard_D2as_v4",
+        max_nodes=1,
+        idle_seconds_before_scaledown=1200, # Scale down after 20 minutes
+    )
+    cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+    # This will block the script until the resource is created
+    cpu_cluster.wait_for_completion(show_output=True)
+
+
+
+src = ScriptRunConfig(
+    source_directory='src/service',
+    script='train.py', 
+    environment=environment,
+    compute_target=cpu_cluster
+    )
     
-
-
-src = ScriptRunConfig(source_directory='src\service', script='train.py', environment=environment)
 run = experiment.submit(src)
 run.wait_for_completion(show_output=True)
